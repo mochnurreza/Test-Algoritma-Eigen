a23e6b6100ef4f135e1b1d89f65d450c61bbd5fc
mochnurreza <nrreza2@gmail.com>
2022-02-12 01:54:11 +0700

    Test Algoritma

--- soal1.js
+++ soal1.js
@@ -1,5 +1,4 @@
 const kata = "NEGIE1";
-// arrange the string to EIGEN1
 const last = kata.substring(kata.length - 1);
 const susun =kata.substring(0,5).split("").reverse().join("");
 const result = susun + last;
